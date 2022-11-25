ea938ebd275ebadc2f20e85c5985ec6dc5a7d4e1
kevin <kmathew@peloton-technologies.com>
2022-11-24 23:39:53 -0800

    adding db to client py

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import socket
 import random
 import threading
+import mongoconfig
 from threading import Thread
 from datetime import datetime
 from colorama import Fore, init, Back
 
 # init colors
 init()
+db = mongoconfig.initializeConnection()
 
 # set the available colors
 colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
@@ -25,19 +27,19 @@ SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5002  # server's port
 separator_token = "<SEP>"  # we will use this to separate the client name & message
 
-#A condition variable to synchronize timings for sending and receiving requests/responses from the server
+# A condition variable to synchronize timings for sending and receiving requests/responses from the server
 responseWait = threading.Condition()
 
-#Request Responses from server are stored here
+# Request Responses from server are stored here
 requestResponse = None
 
-#Keeps track of currently logged in user
+# Keeps track of currently logged in user
 activeUser = None
 
-#Keeps track of which active conversation is currently open
+# Keeps track of which active conversation is currently open
 activeConv = None
 
-#List of active chats
+# List of active chats
 Chats = []
 
 # initialize TCP socket
@@ -47,16 +49,18 @@ print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
 
+
 def main():
 
-    #This thread handles all data recieved from server
+    # This thread handles all data recieved from server
     t = Thread(target=responseHandlerThread)
     t.daemon = True
     t.start()
 
     # Client start up menu
     while True:
-        selection = input("\nPlease select an option\n1.Log in\n2.Create New Account\n3.Exit Client\n")
+        selection = input(
+            "\nPlease select an option\n1.Log in\n2.Create New Account\n3.Exit Client\n")
         if selection == '1':
             if verifyLogin():
                 enterHomePage()
@@ -67,25 +71,26 @@ def main():
             s.close()
             exit()
         else:
-            print("\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
+            print(
+                "\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
 
 
-#Function that handles data sent in from server
+# Function that handles data sent in from server
 def responseHandlerThread():
     global requestResponse
 
-    #Handles when a message is sent from another user
+    # Handles when a message is sent from another user
     def handleMessage(msg):
         parse = msg.split("&-!&&")
         username = parse[0]
         message = parse[1]
 
-        if activeConv == username:       
-            formattedMsg = "\n\t[%s]%s\n" % (username,message)
-            print (formattedMsg) 
-    
-    while True:        
-        msg = s.recv(1024).decode() 
+        if activeConv == username:
+            formattedMsg = "\n\t[%s]%s\n" % (username, message)
+            print(formattedMsg)
+
+    while True:
+        msg = s.recv(1024).decode()
         requestCode = msg[:2]
 
         if requestCode == "&2" and activeConv != None:
@@ -97,9 +102,7 @@ def responseHandlerThread():
             responseWait.release()
 
 
-
-
-#Function that verifies login form server
+# Function that verifies login form server
 def verifyLogin():
     global activeUser
 
@@ -107,21 +110,21 @@ def verifyLogin():
     password = input('Password: ')
 
     response = sync_send(("&3" + username + "&-!&&" + password).encode())
-    
+
     if response[2:] == "True":
-        print ("\nWelcome %s!" % (username))
+        print("\nWelcome %s!" % (username))
         activeUser = username
         return True
-    elif response[2:] == "FalsePassword": 
-        print ('\nError ~ Incorrect Password!\n')
+    elif response[2:] == "FalsePassword":
+        print('\nError ~ Incorrect Password!\n')
         return False
-    else: 
-        print (response)
-        print ('\nError ~ That username does not exist!\n')
+    else:
+        print(response)
+        print('\nError ~ That username does not exist!\n')
         return False
 
 
-#Function that synchronizes with responseHandlerThread to send/receive a request/response from server
+# Function that synchronizes with responseHandlerThread to send/receive a request/response from server
 def sync_send(rqst):
     responseWait.acquire()
 
@@ -130,28 +133,32 @@ def sync_send(rqst):
     responseWait.wait()
     response = requestResponse
     responseWait.release()
-    return response 
+    return response
 
 
-#Client home page (after log-in)
+# Client home page (after log-in)
 def enterHomePage():
-    while True: 
-        selection = input("\nPlease select an option\n1.Chats\n2.Account Settings\n3.Logout\n")
+    while True:
+        selection = input(
+            "\nPlease select an option\n1.Chats\n2.Account Settings\n3.Logout\n")
         if selection == '1':
             openChats()
         elif selection == '2':
             try:
                 openSettings()
-            #In case the user deleted their account    
+            # In case the user deleted their account
             except Exception as e:
                 break
         elif selection == "3":
             logOut()
             return
         else:
-            print("\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
+            print(
+                "\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
 
-#Sends logOut signal to server 
+# Sends logOut signal to server
+
+
 def logOut():
     global activeUser
 
@@ -159,7 +166,7 @@ def logOut():
     activeUser = None
 
 
-#Prototype function that creates a new account for testing menu functionality
+# Prototype function that creates a new account for testing menu functionality
 def createNewAccount():
     global activeUser
 
@@ -169,15 +176,15 @@ def createNewAccount():
     response = sync_send(("&4" + username + "&-!&&" + password).encode())
 
     if response[2:] == "True":
-        print ("\nWelcome %s!" % (username))
+        print("\nWelcome %s!" % (username))
         activeUser = username
         return True
-    else: 
-        print ('\nError ~ Username already exists!\n')
+    else:
+        print('\nError ~ Username already exists!\n')
         return False
 
 
-#Opens active conversations menu 
+# Opens active conversations menu
 def openChats():
 
     def retrieveChats():
@@ -189,20 +196,20 @@ def openChats():
             Chats = []
         else:
             Chats = newChats
-        
-            
-    #Generates the selection menu for active conversations
+
+    # Generates the selection menu for active conversations
+
     def generateSelectionMenu():
         i = 1
         optionMaps = {}
-        print ("\nOpen a chat")
+        print("\nOpen a chat")
 
         for conv in Chats:
-            print("%d.%s" % (i,conv))
+            print("%d.%s" % (i, conv))
             optionMaps[i] = conv
-            i+=1        
+            i += 1
         selection = input("%d.Start a new conversation\nq.Go Back\n" % (i))
-        
+
         if selection == "q":
             return False
 
@@ -214,7 +221,8 @@ def openChats():
             else:
                 enterChatRoom(optionMaps[selection])
         except Exception as e:
-            print("\nError ~ Incorrect input.\n Please enter a number corresponding to a conversation\n")
+            print(
+                "\nError ~ Incorrect input.\n Please enter a number corresponding to a conversation\n")
 
     while True:
         retrieveChats()
@@ -226,44 +234,44 @@ def openChats():
             elif selection == "q":
                 break
             else:
-                print("\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
-          
-        else: 
+                print(
+                    "\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
+
+        else:
             if generateSelectionMenu() == False:
                 break
 
 
-#Function that authenticates a user exists to start a new conversation with them 
+# Function that authenticates a user exists to start a new conversation with them
 def startNewChat():
-    
+
     def verifyUser(user):
-    
+
         response = sync_send(("&1"+user).encode())
-        
+
         if response[2:] == "True":
             return True
         return False
 
-
-    user = input ("\nPlease enter a user's username to chat with: ")
+    user = input("\nPlease enter a user's username to chat with: ")
     if verifyUser(user):
         print("\nYou started a new conversation with %s!\n" % (user))
     else:
         print("\nError ~ User does not exists.\n")
 
 
-#Opens an active chat with a user
+# Opens an active chat with a user
 def enterChatRoom(user):
-    global activeConv 
-    
+    global activeConv
+
     def retrieveHistory():
         response = sync_send(("&7"+user).encode())
         hist = response[2:]
         if hist == "":
-            print ("\nNo message History\n")
+            print("\nNo message History\n")
         else:
             print(response[2:])
-               
+
     retrieveHistory()
     print("(Enter q to exit): ")
     activeConv = user
@@ -275,38 +283,38 @@ def enterChatRoom(user):
         date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         to_send = date_now + ": " + to_send
         s.send(("&2" + user + "&-!&&" + to_send).encode())
-        formattedMsg = "\n\t[%s]%s\n" % (activeUser,to_send)
-        print (formattedMsg)
+        formattedMsg = "\n\t[%s]%s\n" % (activeUser, to_send)
+        print(formattedMsg)
 
     activeConv = None
 
-            
-#Opens user settings menu
+
+# Opens user settings menu
 def openSettings():
 
     def deleteChatHistory(chat):
         sync_send(("&9"+chat).encode())
 
-    #Sends a signal to server to delete activeUser's account
+    # Sends a signal to server to delete activeUser's account
     def deleteAccount():
         global activeUser
 
         sync_send(("&6"+activeUser).encode())
         activeUser = None
-    
-    #Generates menu that allows user to delete currently active chats, history and all (locally for now)
+
+    # Generates menu that allows user to delete currently active chats, history and all (locally for now)
     def generateSelectionMenu():
         i = 1
         optionMaps = {}
-        print ("\nSelect a chat to delete it's history\nNote ~ this will only delete"+\
-                " your copy\n")
+        print("\nSelect a chat to delete it's history\nNote ~ this will only delete" +
+              " your copy\n")
 
         for conv in Chats:
-            print("%d.%s" % (i,conv))
+            print("%d.%s" % (i, conv))
             optionMaps[i] = conv
-            i+=1        
+            i += 1
         selection = input("q.Go Back\n")
-        
+
         if selection == "q":
             return False
 
@@ -315,10 +323,12 @@ def openSettings():
         try:
             deleteChatHistory(optionMaps[selection])
         except Exception as e:
-            print("\nError ~ Incorrect input.\n Please enter a number corresponding to a conversation\n")
+            print(
+                "\nError ~ Incorrect input.\n Please enter a number corresponding to a conversation\n")
 
     while True:
-        selection = input("\nPlease select an option\n1.Delete Message History\n2.Delete Account\nq.Go back\n")
+        selection = input(
+            "\nPlease select an option\n1.Delete Message History\n2.Delete Account\nq.Go back\n")
         if selection == '1':
             if len(Chats) == 0:
                 print("\nYou have no active conversations!\n")
@@ -334,7 +344,8 @@ def openSettings():
         elif selection == "q":
             break
         else:
-            print("\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
+            print(
+                "\nError ~ Incorrect input.\n Please enter a number corresponding to a menu option\n")
 
 
 if __name__ == "__main__":
@@ -360,4 +371,3 @@ while True:
     # finally, send the message
     s.send(to_send.encode())
 """
-
