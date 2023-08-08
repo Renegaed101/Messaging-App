43abe56302681a6a1b15f1c7bff324f78bef9b7f
Mahd Shahzad <mahd123@hotmail.co.uk>
2023-08-08 05:03:35 +0500

    Moved to new database

--- mongoconfig.py
+++ mongoconfig.py
@@ -4,11 +4,11 @@ from pymongo import MongoClient
 def initializeConnection():
     try:
         conn = MongoClient(
-            "mongodb+srv://admin:abcd@cluster0.waeehl7.mongodb.net/?retryWrites=true&w=majority")
+            "mongodb+srv://admin:abcd@cluster0.nkofpby.mongodb.net/?retryWrites=true&w=majority")
         print("Connected successfully!!!")
     except:
         print("Could not connect to MongoDB")
 
     # database name: chatdb
-    db = conn.chatdb
+    db = conn.Messaging_App
     return db
